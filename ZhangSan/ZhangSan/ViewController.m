4cd36cdeeef0da3eaf53f6e13a43878a2e5835a3
lan01 <123456@.qq.com>
2015-08-14 10:48:14 +0800

    添加33333333

--- ZhangSan/ZhangSan/ViewController.m
+++ ZhangSan/ZhangSan/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"11111111111");
+    NSLog(@"22222222222");
 }
 
 - (void)didReceiveMemoryWarning {
